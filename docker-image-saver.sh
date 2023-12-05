4afc2fa794d5488d925fdcbbcec46a5890149bd4
farmboy-dev <70833564+farmboy-dev@users.noreply.github.com>
2023-12-05 15:19:12 -0600

    Update docker-image-saver.sh

--- docker-image-saver.sh
+++ docker-image-saver.sh
@@ -2,11 +2,11 @@
 
 # Help function
 show_help() {
-    echo "Usage: $0 <IMAGE_LIST_FILE> <SAVE_DIR>"
+    echo "Usage: $0 [OPTIONS] [IMAGE_LIST_FILE] [SAVE_DIR]"
     echo "Download Docker images listed in IMAGE_LIST_FILE, save them as tar files, and remove the images."
     echo "Arguments:"
-    echo "  IMAGE_LIST_FILE  Path to the file containing Docker image names."
-    echo "  SAVE_DIR         Directory to save Docker images as tar files."
+    echo "  IMAGE_LIST_FILE  Path to the file containing Docker image names. If not provided, images can be typed directly."
+    echo "  SAVE_DIR         Directory to save Docker images as tar files. If not provided, current working directory will be used."
     echo "Options:"
     echo "  -h, --help       Show this help message and exit."
 }
@@ -25,30 +25,29 @@ case "$1" in
         ;;
 esac
 
-# Set the path and filename for the image list
-IMAGE_LIST_FILE="$1"
-
-# Set the directory to save images
-SAVE_DIR="$2"
-
-# Check for required arguments
-if [ -z "$IMAGE_LIST_FILE" ] || [ -z "$SAVE_DIR" ]; then
-    echo "Error: Missing required arguments."
-    show_help
-    exit 1
+# Check if IMAGE_LIST_FILE is provided
+if [ -f "$1" ]; then
+    # Read the image list from the file
+    IMAGE_LIST_FILE="$1"
+    SAVE_DIR="${2:-.}"  # Use current working directory if SAVE_DIR is not provided
+else
+    # Read the image list from the arguments
+    IMAGE_LIST_FILE=""
+    SAVE_DIR="${1:-.}"  # Use current working directory if SAVE_DIR is not provided
 fi
 
-# Check if the image list file exists
-if [ ! -f "$IMAGE_LIST_FILE" ]; then
-    echo "Error: IMAGE_LIST_FILE not found."
-    exit 1
-fi
-
-# Create the save directory
+# Create the save directory if it doesn't exist
 mkdir -p "$SAVE_DIR"
 
-# Read the image list file and download, save, and remove each image
-while IFS= read -r image; do
+# Read the image list from either file or arguments
+if [ -n "$IMAGE_LIST_FILE" ]; then
+    IMAGES=$(cat "$IMAGE_LIST_FILE")
+else
+    IMAGES="${@:2}"
+fi
+
+# Process each image: download, save, and remove
+for image in $IMAGES; do
     echo "Processing image: $image"
     
     # Download the image
@@ -61,8 +60,6 @@ while IFS= read -r image; do
     docker rmi "$image"
     
     echo "Image $image has been saved and removed."
-    
-done < "$IMAGE_LIST_FILE"
+done
 
 echo "Process completed. Images are saved in $SAVE_DIR directory."
-
