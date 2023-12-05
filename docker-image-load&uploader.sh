d418a382a75252102ab6262b8db4f7bbaa588720
farmboy-dev <70833564+farmboy-dev@users.noreply.github.com>
2023-12-05 16:34:52 -0600

    Update docker-image-load&uploader.sh

--- docker-image-load&uploader.sh
+++ docker-image-load&uploader.sh
@@ -3,12 +3,13 @@
 # Help function
 show_help() {
     echo "Usage: $0 [OPTIONS] [TAR_DIR_OR_FILE] [DOCKER_REGISTRY]"
-    echo "Load Docker images from tar files in TAR_DIR_OR_FILE, tag them with DOCKER_REGISTRY, and push to the registry."
+    echo "Load Docker images from tar files in TAR_DIR_OR_FILE, tag them with DOCKER_REGISTRY, push to the registry, and optionally remove the local images."
     echo "Arguments:"
     echo "  TAR_DIR_OR_FILE  Directory containing Docker image tar files or a single tar file."
     echo "  DOCKER_REGISTRY  Docker registry address to tag the images. Example: kubekey.cluster.local/test"
     echo "Options:"
     echo "  -h, --help       Show this help message and exit."
+    echo "  -r, --remove     Remove the local images after pushing to the registry."
 }
 
 # Show help if no parameters are provided
@@ -44,39 +45,42 @@ else
     exit 1
 fi
 
-# Check if TAR_DIR_OR_FILE is a directory or a file
-if [ -d "$TAR_DIR_OR_FILE" ]; then
-    # If it's a directory, loop through tar files in TAR_DIR
-    for tar_file in "$TAR_DIR_OR_FILE"/*.tar; do
-        process_tar_file "$tar_file" "$DOCKER_REGISTRY"
-    done
-elif [ -f "$TAR_DIR_OR_FILE" ]; then
-    # If it's a single tar file, process it
-    process_tar_file "$TAR_DIR_OR_FILE" "$DOCKER_REGISTRY"
-else
-    echo "Error: Invalid TAR_DIR_OR_FILE provided."
-    show_help
-    exit 1
-fi
+# Check for the remove option
+REMOVE_OPTION=false
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -r|--remove)
+            REMOVE_OPTION=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
-echo "Process completed."
-
-# Function to process a single tar file
-process_tar_file() {
-    local tar_file="$1"
-    local registry="$2"
-
+# Loop through tar files in TAR_DIR_OR_FILE
+for tar_file in "$TAR_DIR_OR_FILE"/*.tar; do
     # Extract image name from tar file
-    local image_name=$(docker inspect --format="{{.RepoTags}}" "$(docker load -i "$tar_file" | awk '{print $NF}' | sed 's/:$//')")
+    image_name=$(docker inspect --format="{{.RepoTags}}" "$(docker load -i "$tar_file" | awk '{print $NF}' | sed 's/:$//')")
 
     # Remove 'XXX.io/' from the beginning of the image name
     image_name=$(echo "$image_name" | sed -E 's/^[[:alnum:]]+\.io\///')
 
     # Tag the image with the Docker registry address
-    docker tag "$image_name" "$registry/$image_name"
+    docker tag "$image_name" "$DOCKER_REGISTRY/$image_name"
 
     # Push the tagged image to the registry
-    docker push "$registry/$image_name"
+    docker push "$DOCKER_REGISTRY/$image_name"
 
-    echo "Image $image_name has been tagged and pushed to $registry."
-}
+    echo "Image $image_name has been tagged and pushed to $DOCKER_REGISTRY."
+
+    if [ "$REMOVE_OPTION" = true ]; then
+        # Remove the local image
+        docker rmi "$image_name"
+        echo "Local image $image_name has been removed."
+    fi
+done
+
+echo "Process completed."
+
